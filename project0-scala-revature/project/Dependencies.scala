8aac959466efd30dfa1998b73b0ec739167fd70b
granpawalton <36310010+granpawalton@users.noreply.github.com>
2020-12-15 15:21:50 -0600

    Now reads JSON files

--- project0-scala-revature/project/Dependencies.scala
+++ project0-scala-revature/project/Dependencies.scala
@@ -2,4 +2,5 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.2"
+  lazy val play = "com.typesafe.play" %% "play-json" % "2.8.0"
 }
--- project0-scala-revature/src/main/scala/example/Hello.scala
+++ project0-scala-revature/src/main/scala/example/Hello.scala
@@ -1,9 +1,11 @@
 package example
 
+import play.api.libs.json._
+import scala.io.Source
+
 object Hello {
   def main(args: Array[String]) {
-    for(arg<-args) {
-      println(arg)
-    }
+    var file = io.Source.fromFile(args(0))
+    println((Json.parse(file.mkString) \ "name").get)
   }
 }
