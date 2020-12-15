7ea0d90f5d616fc069857053e87f0e422e07b3e7
granpawalton <36310010+granpawalton@users.noreply.github.com>
2020-12-15 15:25:27 -0600

    Error catching for file reading

--- project0-scala-revature/src/main/scala/example/Hello.scala
+++ project0-scala-revature/src/main/scala/example/Hello.scala
@@ -5,7 +5,12 @@ import scala.io.Source
 
 object Hello {
   def main(args: Array[String]) {
-    var file = io.Source.fromFile(args(0))
-    println((Json.parse(file.mkString) \ "name").get)
+    try {
+      var file = io.Source.fromFile(args(0))
+      println((Json.parse(file.mkString) \ "name").get)
+    } catch {
+      case e: java.io.FileNotFoundException => println("Couldn't find that file.")
+      case e: java.io.IOException => println("Had an IOException trying to read that file")
+    }
   }
 }
