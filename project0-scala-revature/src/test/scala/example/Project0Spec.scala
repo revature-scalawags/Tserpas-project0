c5005875feba57273bce620f5e51d0f7e4491699
granpawalton <36310010+granpawalton@users.noreply.github.com>
2020-12-22 14:55:30 -0600

    minimum value product

--- project0-scala-revature/src/test/scala/example/Project0Spec.scala
+++ project0-scala-revature/src/test/scala/example/Project0Spec.scala
@@ -1,9 +1,9 @@
-package example
+package wowServerTracker
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class HelloSpec extends AnyFlatSpec with Matchers {
+class Project0Spec extends AnyFlatSpec with Matchers {
   "The Hello object" should "say hello" in {
     Hello.greeting shouldEqual "hello"
   }
